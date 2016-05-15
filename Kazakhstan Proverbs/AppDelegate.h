2bd9d1adedc2c80e2cae34dbe586c760c6f2495f
admin <admin@macbookpro.local>
2016-05-14 23:10:31 -0700

    Initial Commit

---
+++ Kazakhstan Proverbs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kazakhstan Proverbs
+//
+//  Created by admin on 2016-05-14.
+//  Copyright © 2016 Maxim Puchkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kazakhstan Proverbs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kazakhstan Proverbs
+//
+//  Created by admin on 2016-05-14.
+//  Copyright © 2016 Maxim Puchkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
